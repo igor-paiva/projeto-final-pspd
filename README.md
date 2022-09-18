f5da227ff33efb7a909c8acf4aef5393c81d757f
igor-paiva <igorbatistapaiva@outlook.com>
2022-09-18 13:53:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Parte 2 - Spark Streaming contabilizando palavras via Apache Kafka
+
+Igor Batista Paiva - 18/0018728
+
+## Sobre
+
+Para executar será necessário ter instalado o `docker` e `docker-compose`.
+
+## Configuração
+
+Para instalar todas as dependências e criar todos os elementos necessários utilize o comando abaixo:
+
+```
+bash setup.sh
+```
+
+## Como executar
+
+Após ter rodado o *setup* com sucesso para iniciar novamente só é necessário subir os containers e seguir os demais passos:
+
+```
+docker-compose up -d
+```
+
+Os containers sobem em modo *detach*, portanto caso queira ver os logs dos containers utilize:
+
+```
+docker-compose logs -f
+```
+
+### Iniciar o master
+
+Será necessário iniciar o spark master:
+
+```
+docker exec -it spark-master bash spark-3.1.3-bin-hadoop3.2/sbin/start-master.sh
+```
+
+### Iniciar os workers
+
+Para iniciar os *workers*:
+
+```
+docker exec -it spark-worker-1 bash spark-3.1.3-bin-hadoop3.2/sbin/start-worker.sh -m 1G -c 1 spark://spark-master:7077 && docker exec -it spark-worker-2 bash spark-3.1.3-bin-hadoop3.2/sbin/start-worker.sh -m 1G -c 1 spark://spark-master:7077
+```
+
+### Iniciar o publisher
+
+O publisher envia eventos para o servidor Kafka a cada meio segundo (0,5 segundo) com um paragrafo de texto.
+
+Em um novo terminal (a chamada é blocante):
+
+```
+docker exec -it kafka-publisher bash
+
+# dentro do container
+python3 -u main.py
+```
+
+Para encerrar: `CTRL + C`.
+
+### Submeter o problema
+
+Em um novo terminal entre no container do spark driver:
+
+```
+docker exec -it spark-driver bash
+```
+
+Para submeter o problema utilize um dos comandos abaixo, o arquivo txt no final é para onde os resultados serão salvos.
+
+#### Local
+
+Para o modo local não é necessário iniciar o spark master e workers.
+
+```
+./spark-3.1.3-bin-hadoop3.2/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3 local.py > results.txt
+```
+
+#### Cluster
+
+```
+./spark-3.1.3-bin-hadoop3.2/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3 cluster.py > results.txt
+```
+
+## Resultados
+
+Os resultados serão listados como tabelas com nomes de colunas descritivos no arquivo texto fornecido como saída para o comando `spark-submit`.
---
+++ driver/elastic.py
@@ -0,0 +1,197 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import (
+    explode,
+    split,
+    initcap,
+    count,
+    length,
+    window,
+    col,
+    lit,
+    udf,
+    regexp_replace,
+)
+from pyspark.sql.types import BooleanType
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+
+MAX_NUM_ROWS = 2147483647
+
+ARTICLES = [
+    "a",
+    "an",
+    "the",
+]
+
+CONJUNCTIONS = [
+    "for",
+    "and",
+    "nor",
+    "but",
+    "or",
+    "yet",
+    "so",
+]
+
+COMMON_PREPOSITIONS = [
+    "aboard",
+    "about",
+    "above",
+    "across",
+    "after",
+    "against",
+    "along",
+    "amid",
+    "among",
+    "anti",
+    "around",
+    "as",
+    "at",
+    "before",
+    "behind",
+    "below",
+    "beneath",
+    "beside",
+    "besides",
+    "between",
+    "beyond",
+    "but",
+    "by",
+    "concerning",
+    "considering",
+    "despite",
+    "down",
+    "during",
+    "except",
+    "excepting",
+    "excluding",
+    "following",
+    "for",
+    "from",
+    "in",
+    "inside",
+    "into",
+    "like",
+    "minus",
+    "near",
+    "of",
+    "off",
+    "on",
+    "onto",
+    "opposite",
+    "outside",
+    "over",
+    "past",
+    "per",
+    "plus",
+    "regarding",
+    "round",
+    "save",
+    "since",
+    "than",
+    "through",
+    "to",
+    "toward",
+    "towards",
+    "under",
+    "underneath",
+    "unlike",
+    "until",
+    "up",
+    "upon",
+    "versus",
+    "via",
+    "with",
+    "within",
+    "without",
+]
+
+WORDS_TO_FILTER = ARTICLES + CONJUNCTIONS + COMMON_PREPOSITIONS
+
+es = Elasticsearch("http://elasticsearch:9200")
+
+spark = (
+    SparkSession.builder.master("spark://spark-master:7077")
+    .appName("StreamingWordCountKafkaDistributed")
+    .config("spark.driver.host", "spark-driver")
+    # .config("spark.driver.port", "5005")
+    .config("spark.dynamicAllocation.enabled", "false")
+    .config("spark.shuffle.service.enabled", "false")
+    .config("spark.streaming.driver.writeAheadLog.closeFileAfterWrite", "true")
+    .config("spark.streaming.receiver.writeAheadLog.closeFileAfterWrite", "true")
+    .config("spark.executor.memory", "512m")
+    .config("spark.executor.instances", "2")
+    .config("spark.pyspark.python", "python3")
+    .config("spark.pyspark.driver.python", "python3")
+    .config("spark.sql.debug.maxToStringFields", "100")
+    .getOrCreate()
+)
+
+# spark_context = spark.sparkContext
+# spark_context.checkpoint("hdfs://hadoop:9000/checkpoint")
+
+lines = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka-server:9092")
+    # .option("kafka.security.protocol", "SSL")
+    .option("failOnDataLoss", "false")
+    .option("subscribe", "sentences")
+    .option("includeHeaders", "true")
+    .option("startingOffsets", "earliest")  # we could use latest
+    .option("spark.streaming.kafka.maxRatePerPartition", "50")
+    .load()
+)
+
+words = (
+    lines.select("timestamp", explode(split(lines.value, " ")).alias("word"))
+    .withColumn("word", regexp_replace(col("word"), r"[^a-zA-Z'-]", ""))
+    .filter(length(col("word")) > 1)
+)
+
+
+def filter_starts_with(word, letter):
+    return word.capitalize().startswith(letter)
+
+
+def filter_by_size(word, length):
+    return len(word) == length
+
+
+def func(batch_df, batch_id):
+    print(f"\nBatch ID: {batch_id}\n")
+    print(f"\n\tReceived words: {len(batch_df.count())}\n")
+
+    words_list = list(
+        filter(
+            lambda word: word.lower() not in WORDS_TO_FILTER,
+            map(lambda row: row.word, batch_df.collect()),
+        )
+    )
+
+    starts_with_s = list(filter(lambda word: filter_starts_with(word, "S"), words_list))
+    starts_with_r = list(filter(lambda word: filter_starts_with(word, "R"), words_list))
+    starts_with_p = list(filter(lambda word: filter_starts_with(word, "P"), words_list))
+
+    size_6 = list(filter(lambda word: filter_by_size(word, 6), words_list))
+    size_8 = list(filter(lambda word: filter_by_size(word, 8), words_list))
+    size_11 = list(filter(lambda word: filter_by_size(word, 11), words_list))
+
+    es.index(
+        id=batch_id,
+        index="words",
+        document={
+            "words": words_list,
+            "total_words": len(words_list),
+            "starts_with_s": starts_with_s,
+            "starts_with_r": starts_with_r,
+            "starts_with_p": starts_with_p,
+            "size_6": size_6,
+            "size_8": size_8,
+            "size_11": size_11,
+            "timestamp": datetime.now(),
+        },
+    )
+
+
+words.writeStream.foreachBatch(func).start().awaitTermination()
